ce3f4bbf4cf1670bbc7abdd98382b69d2b06c50a
SAHIL BHARDWAJ <krishnabhardwaj200517@gmail.com>
2025-10-22 17:23:23 +0530

    learning params regex and other things

--- src/app.js
+++ src/app.js
@@ -2,22 +2,9 @@ const express = require("express");
 
 const app = express();
 
-//This will only handle GET call to /user
 app.get("/user", (req, res) => {
-  res.send({ firstName: "Sahil", LastName: "Bhardwaj" });
-});
-
-app.post("/user", (req, res) => {
-  res.send("Data Saved to the Database");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Data Deleted Successfully");
-});
-
-//This will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-  res.send({ firstName: "Sahil", LastName: "Bhardwaj" });
+  console.log(req.params);
+  res.send({ firstName: "Sahil", lastName: "Bhardwaj" });
 });
 
 app.listen(3000, () => {
