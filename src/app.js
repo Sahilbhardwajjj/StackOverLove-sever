246e7447a332eba9a72385fb80c0fecf4b587161
SAHIL BHARDWAJ <krishnabhardwaj200517@gmail.com>
2025-10-25 17:09:58 +0530

    Connecting database to the server

--- src/app.js
+++ src/app.js
@@ -1,17 +1,15 @@
 const express = require("express");
-const { adminAuth } = require("./middlewares/auth");
+const { dbConnect } = require("./config/database");
+
 const app = express();
 
-app.get("/user", (req, res) => {
-  try {
-    throw new Error("Error Occured");
-  } catch (err) {
-    console.log(err.message);
-    console.log(err.stack);
-    res.status(500).send("Error");
-  }
-});
-
-app.listen(3000, () => {
-  console.log("Server is listening to port 3000");
-});
+dbConnect()
+  .then(() => {
+    console.log("Database connected successfully");
+    app.listen(3000, () => {
+      console.log("Server is listening to port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log("Database Cannot be Establisheds");
+  });
---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const dbConnect = async () => {
+  await mongoose.connect(
+    "mongodb+srv://sahil:sahil123@stackoverlove.n5ziqnh.mongodb.net/"
+  );
+};
+
+module.exports = dbConnect;
