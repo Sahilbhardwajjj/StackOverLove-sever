81e6ee505eddffb4ff7a427414f4d036ea18d704
SAHIL BHARDWAJ <krishnabhardwaj200517@gmail.com>
2025-10-31 17:20:04 +0530

    update

--- src/models/user.js
+++ src/models/user.js
@@ -3,63 +3,66 @@ const { type } = require("os");
 const { Schema } = mongoose;
 const validator = require("validator");
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    minLength: 3,
-  },
-  firstName: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    validate: {
-      validator: function (value) {
-        return validator.isEmail(value);
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      minLength: 3,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      validate: {
+        validator: function (value) {
+          return validator.isEmail(value);
+        },
+        message: "Email Address is invalid",
       },
-      message: "Email Address is invalid",
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: [5, "Password too short"],
+    },
+    dateOfBirth: {
+      type: Date,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+    },
+    role: {
+      type: String,
+      enum: ["admin", "moderator", "user"],
+      default: "user",
+    },
+    bio: {
+      type: String,
+      default: "Default about of user",
+    },
+    skills: {
+      type: [String],
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: [18, "Age is less that required"],
+      max: [70, "Age is too big , you should do meditation"],
     },
   },
-  password: {
-    type: String,
-    required: true,
-    minLength: [5, "Password too short"],
-  },
-  dateOfBirth: {
-    type: Date,
-  },
-  gender: {
-    type: String,
-    enum: ["male", "female"],
-  },
-  role: {
-    type: String,
-    enum: ["admin", "moderator", "user"],
-    default: "user",
-  },
-  bio: {
-    type: String,
-    default: "Default about of user",
-  },
-  skills: {
-    type: [String],
-  },
-  age: {
-    type: Number,
-    required: true,
-    min: [18, "Age is less that required"],
-    max: [70, "Age is too big , you should do meditation"],
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
