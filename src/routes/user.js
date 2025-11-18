8d8b40fd311f12d5a1c4d0868018fe14a0bddc0d
SAHIL BHARDWAJ <krishnabhardwaj200517@gmail.com>
2025-11-18 18:14:55 +0530

    Completed using pagnition on feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -63,6 +63,10 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 userRouter.get("/user/feed", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
 
     // Getting all connectionRequest where loggedInUser is present in "fromUserId" and "toUserId"
     const connectionRequests = await ConnectionRequestModel.find({
@@ -79,7 +83,10 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
     // Fetching User which are not present in hideUserFromFeed
     const usersOnFeed = await User.find({
       _id: { $nin: [...hideUserFromFeed], $ne: loggedInUser._id },
-    }).select("firstName lastName");
+    })
+      .select("firstName lastName")
+      .skip(skip)
+      .limit(limit);
 
     res.status(200).json({
       message: "Feed of User",
