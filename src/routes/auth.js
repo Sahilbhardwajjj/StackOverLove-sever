782a91d469095e31464606af1a4919c2f34b2d00
SAHIL BHARDWAJ <krishnabhardwaj200517@gmail.com>
2026-01-28 23:02:28 +0530

    changed auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -84,6 +84,8 @@ authRouter.post("/login", async (req, res) => {
       res.cookie("token", token, {
         expires: new Date(Date.now() + 8 * 3600000),
         httpOnly: true,
+        sameSite: "none",
+        secure: true,
       });
 
       res.status(200).json({
